31a0c920ffe7d328f2f61107868805b6c73047f0
Abror Ruziboev <abror.ruzibayev@gmail.com>
2024-04-12 11:16:31 +0200

    atlas.sh fix

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 curl -sSf https://atlasgo.sh > atlas.sh
+chmod +x ./atlas.sh
 ./atlas.sh
 
 atlas migrate apply -u "postgres://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME?sslmode=disable"
